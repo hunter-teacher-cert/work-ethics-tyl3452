69a2e9c7022cf9c61d8885a3187ba10d20c23a5e
tyl3452 <tayou.liu@gmail.com>
2021-11-02 15:32:20 -0400

    change to file name and started breaking algo up

--- week_07_regex/regex.py
+++ week_07_regex/regex.py
@@ -3,21 +3,33 @@
 import re
 
 
-def find_date(line):
-    pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
-    result = re.findall(pattern,line)
+def find_name(line):
 
-    pattern = r"(October|Oct|November|Nov)( \d{1,2}, \d{4})"
-    result = result + re.findall(pattern,line)
+    # Decided to separate out seaches
+
+    # If there is a title in the name
+    title_pattern = r"(Mr|Mrs|Ms|Mx|Dr)."
+    result = re.findall(title_pattern,line)
+
+    # If there is no title and has First + Last Name
+    first_last_pattern = r""
+
+    # If there is no title + Last name
+    last_pattern = r""
+
+
+    # pattern = r"(October|Oct|November|Nov)( \d{1,2}, \d{4})"
+    # result = result + re.findall(pattern,line)
     return result
 
 
 #result = find_date("10/15/2023 is a October 13, 2025 date as is 1/23/19")
 #print(result)
 
-f = open("datefile.dat")
+
+f = open("datafile.dat")
 for line in f.readlines():
-    #print(line)
-    result = find_date(line)
+    print(line)
+    result = find_name(line)
     if (len(result)>0):
         print(result)
